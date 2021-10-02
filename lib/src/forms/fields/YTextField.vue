c60e2d7fdae3db18f9818cb9c32353d300c7a3f0
Mathieu Marteau <marteau.mathieu@gmail.com>
2021-10-02 20:46:51 +0200

    Update TextField, extends original VTextField

--- lib/src/forms/fields/YTextField.vue
+++ lib/src/forms/fields/YTextField.vue
@@ -1,19 +1,21 @@
-<template>
-  <v-text-field
-    v-bind="$attrs"
-    outlined
-    dense
-    :name="`${new Date().getTime()}-text-input`"
-    v-on="$listeners"
-  >
-    <!-- pass through scoped slots -->
-    <template v-for="(_, scopedSlotName) in $scopedSlots" #[scopedSlotName]="slotData">
-      <slot :name="scopedSlotName" v-bind="slotData" />
-    </template>
-
-    <!-- pass through normal slots -->
-    <template v-for="(_, slotName) in $slots" #[slotName]>
-      <slot :name="slotName" />
-    </template>
-  </v-text-field>
-</template>
+<script>
+import { VTextField } from 'vuetify/lib'
+export default {
+  extends: VTextField,
+  props: {
+    outlined: {
+      type: Boolean,
+      default: true
+    },
+    dense: {
+      type: Boolean,
+      default: true
+    }
+  },
+  created () {
+    if (this.attrs$ && !this.attrs$.name) {
+      this.attrs$.name = this.$attrs.name || `${new Date().getTime()}-text-input`
+    }
+  }
+}
+</script>
