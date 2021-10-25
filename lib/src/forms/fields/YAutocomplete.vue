71d736e088a2cc3c742c8a18ca873dd7bc7c3062
Mathieu Marteau <marteau.mathieu@gmail.com>
2021-10-25 12:16:53 +0200

    Added autocomplete field

---
+++ lib/src/forms/fields/YAutocomplete.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-autocomplete
+      v-bind="$attrs"
+      outlined
+      dense
+      v-on="$listeners"
+  >
+    <template v-for="(_, slotName) in $slots" #[slotName]>
+      <slot :name="slotName" />
+    </template>
+    <template v-for="(_, scopedSlotName) in $scopedSlots" #[scopedSlotName]="slotData">
+      <slot :name="scopedSlotName" v-bind="slotData" />
+    </template>
+  </v-autocomplete>
+</template>
